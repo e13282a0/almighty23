2a0baf0f167a82694051a42bd041f5ddf229c263
Christian <christian.rust@gmx.com>
2023-01-08 21:28:50 +0100

    major separator

--- src/components/ThePlanner.vue
+++ src/components/ThePlanner.vue
@@ -1,8 +1,8 @@
 <template>
 
-    <svg :width="state.width" :height = "state.height" >
+    <svg :width="state.width" :height = "state.height" class="planner">
       <g id="grid">
-        <line v-for="(elm, index) in state.timeBeam" :x1="(index*state.colWidth)+state.namesColumnWidth" y1="0" :x2="(index*state.colWidth)+state.namesColumnWidth" :y2="state.height" stroke="black" />
+        <line v-for="(elm, index) in state.timeBeam" :class=" {'grid-lines__head--major':elm.majorSeparator,'grid-lines__head--minor':!elm.majorSeparator,}" :x1="(index*state.colWidth)+state.namesColumnWidth" y1="0" :x2="(index*state.colWidth)+state.namesColumnWidth" :y2="state.height" stroke="black" />
         <line v-for="(elm, index) in issues" x1="0" :y1="(index*state.rowHeight)+state.headerHeight" :x2="state.width" :y2="(index*state.rowHeight)+state.headerHeight" stroke="black" />
       </g>
       <g id="headline">
@@ -28,11 +28,11 @@ export default {
   props:["issues"],
   setup(props) {
     let colWith = 14
-    let namesColumnWidth = 100
+    let namesColumnWidth = 300
     let headerHeight=50
     let rowHeight=30
     let timeBeam = makeTimeBeam()
-
+    debugger
     const state= reactive({
       colWidth:colWith,
       namesColumnWidth:namesColumnWidth,
@@ -49,5 +49,24 @@ export default {
 </script>
 
 <style scoped>
+.planner {
+  padding: 10px;
+}
 
+.grid-lines__head--minor{
+  stroke-dasharray: 5 3;
+  stroke-width: 1px;
+  stroke: #aaaaaa;
+
+}
+.grid-lines__head--major{
+  stroke-width: 1px;
+  stroke: #000000;
+}
+.grid-lines__body--minor{
+
+}
+.grid-lines__body--major{
+
+}
 </style>
