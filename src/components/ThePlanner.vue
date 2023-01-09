30ac3de39e5e132cd379cf05f5548147b752a501
Christian <christian.rust@gmx.com>
2023-01-09 21:09:48 +0100

    dates added

--- src/components/ThePlanner.vue
+++ src/components/ThePlanner.vue
@@ -6,8 +6,12 @@
         <line v-for="(elm, index) in issues" x1="0" :y1="(index*state.rowHeight)+state.headerHeight" :x2="state.width" :y2="(index*state.rowHeight)+state.headerHeight" stroke="black" />
       </g>
       <g id="headline">
-        <g id="empty"></g>
-        <g id="grid"></g>
+        <g v-for="(elm, index) in state.timeBeam">
+          <text v-if="elm.newYear" class="grid_font__head--day" :x="(index*state.colWidth)+state.namesColumnWidth" y="10">{{ formatDateTime(elm.startDate,"YY") }} </text>
+          <text v-if="elm.newMonth" class="grid_font__head--day" :x="(index*state.colWidth)+state.namesColumnWidth" y="20">{{ formatDateTime(elm.startDate,"MMM") }} </text>
+          <text class="grid_font__head--day" :x="(index*state.colWidth)+state.namesColumnWidth" y="30">{{ formatDateTime(elm.startDate,"DD") }} </text>
+          <text class="grid_font__head--day" :x="(index*state.colWidth)+state.namesColumnWidth" y="40">{{ formatDateTime(elm.startDate,"dd") }} </text>
+          </g>
       </g>
       <g id="body">
         <g id="names"></g>
@@ -22,12 +26,13 @@
 <script>
 import { reactive } from 'vue';
 import { makeTimeBeam } from '@/mixins/timebeam'
+import moment from "moment";
 
 export default {
   name: "ThePlanner",
   props:["issues"],
   setup(props) {
-    let colWith = 14
+    let colWith = 16
     let namesColumnWidth = 300
     let headerHeight=50
     let rowHeight=30
@@ -42,7 +47,11 @@ export default {
       height:headerHeight+(props.issues.length*rowHeight),
       timeBeam:timeBeam,
     })
-  return {state}
+
+    function formatDateTime(value, format="YYYY-MM-DD") {
+      return moment(value).format(format);
+    }
+  return {state,formatDateTime}
   }
 
 }
@@ -62,6 +71,10 @@ export default {
 .grid-lines__head--major{
   stroke-width: 1px;
   stroke: #000000;
+}
+
+.grid_font__head--day{
+ font-size: 10px;
 }
 .grid-lines__body--minor{
 
